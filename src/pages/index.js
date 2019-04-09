00159537a9c9e56e32b18c57c492fe440b20b0df
Holger Mueller <9jholger@gmail.com>
2019-04-09 16:29:30 -0500

    scaffold form pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,31 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-export default class IndexPage extends React.Component{
+export default class IndexPage extends React.Component {
   state = {
     username: ``,
-    password:``,
+    password: ``,
   }
-  render(){
-    return(
+  render() {
+    return (
       <Layout>
-      <SEO title="HOME" />
-      <h1>Welcome to Call On Me!</h1>
-      <h1>Log in here:</h1>
-      <h5>Log in goes here.</h5>
-      <h2>Not registered?</h2>
-      <Link to="/signup/">Sign up here!</Link>
-      <br />
-      <Link to="/profile/">See profile page.</Link>
-      <Link to="/class/">See class page.</Link>
-    </Layout>
+        <SEO title="HOME" />
+        <h1>Welcome to Call On Me!</h1>
+        <h1>Log in here:</h1>
+        <h5>Log in goes here.</h5>
+        <form method="post">
+          <input type="text" placeholder="Username" />
+          <input type="text" placeholder="Password" />
+          <input type="submit" value="Log In" />
+        </form>
+        <h2>Not registered?</h2>
+        <Link to="/signup/">Sign up here!</Link>
+        <br />
+        <Link to="/profile/">See profile page.</Link>
+        <Link to="/class/">See class page.</Link>
+      </Layout>
     )
   }
 }
-
-
-
-
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -3,12 +3,24 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SignUpPage = () => (
-  <Layout>
-    <SEO title="Sign-Up" />
-    <h1>Register Here!</h1>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
-
-export default SignUpPage
+export default class SignUpPage extends React.Component {
+  state = {}
+  render() {
+    return (
+      <Layout>
+        <SEO title="Sign-Up" />
+        <h1>Register Here!</h1>
+        <form method="post">
+          <input type="text" placeholder="Enter first name" />
+          <input type="text" placeholder="Enter last name" />
+          <input type="text" placeholder="Enter username" />
+          <input type="email" placeholder="Enter email" />
+          <input type="password" placeholder="Enter password" />
+          <input type="password" placeholder="Confirm password" />
+          <input type="submit" value="Register" />
+        </form>
+        <Link to="/">Go back to the homepage</Link>
+      </Layout>
+    )
+  }
+}
