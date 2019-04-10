1cb5d406b42da72be6997882c5763ac4cff86623
Holger Mueller <9jholger@gmail.com>
2019-04-09 19:49:48 -0500

    forms up and working

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,21 +4,64 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 export default class IndexPage extends React.Component {
-  state = {
-    username: ``,
-    password: ``,
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: ``,
+      password: ``,
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this)
   }
+
+  handleChange = e => {
+    const target = e.target
+    const value = target.value
+    const name = target.name
+
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  handleLoginSubmit(e) {
+    e.preventDefault()
+    console.log(this.state)
+
+    this.setState({
+      username: ``,
+      password: ``,
+    })
+  }
+
   render() {
     return (
       <Layout>
         <SEO title="HOME" />
         <h1>Welcome to Call On Me!</h1>
         <h1>Log in here:</h1>
-        <h5>Log in goes here.</h5>
+        <hr />
         <form method="post">
-          <input type="text" placeholder="Username" />
-          <input type="text" placeholder="Password" />
-          <input type="submit" value="Log In" />
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+          <input
+            type="submit"
+            value="Log In"
+            onClick={this.handleLoginSubmit}
+          />
         </form>
         <h2>Not registered?</h2>
         <Link to="/signup/">Sign up here!</Link>
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -4,20 +4,99 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 export default class SignUpPage extends React.Component {
-  state = {}
+  constructor(props) {
+    super(props)
+    this.state = {
+      firstName: ``,
+      lastName: ``,
+      userName: ``,
+      email: ``,
+      password: ``,
+      confirmPassword: ``,
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleRegistrationSubmit = this.handleRegistrationSubmit.bind(this)
+  }
+
+  handleChange = e => {
+    const target = e.target
+    const value = target.value
+    const name = target.name
+
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  handleRegistrationSubmit = e => {
+    e.preventDefault()
+    console.log(this.state)
+
+    this.setState({
+      firstName: ``,
+      lastName: ``,
+      userName: ``,
+      email: ``,
+      password: ``,
+      confirmPassword: ``,
+    })
+  }
+
   render() {
     return (
       <Layout>
         <SEO title="Sign-Up" />
         <h1>Register Here!</h1>
+        <hr />
         <form method="post">
-          <input type="text" placeholder="Enter first name" />
-          <input type="text" placeholder="Enter last name" />
-          <input type="text" placeholder="Enter username" />
-          <input type="email" placeholder="Enter email" />
-          <input type="password" placeholder="Enter password" />
-          <input type="password" placeholder="Confirm password" />
-          <input type="submit" value="Register" />
+          <input
+            type="text"
+            name="firstName"
+            placeholder="Enter first name"
+            value={this.state.firstName}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Enter last name"
+            value={this.state.lastName}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="userName"
+            placeholder="Enter username"
+            value={this.state.userName}
+            onChange={this.handleChange}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm password"
+            value={this.state.confirmPassword}
+            onChange={this.handleChange}
+          />
+          <input
+            type="submit"
+            value="Register"
+            onClick={this.handleRegistrationSubmit}
+          />
         </form>
         <Link to="/">Go back to the homepage</Link>
       </Layout>
