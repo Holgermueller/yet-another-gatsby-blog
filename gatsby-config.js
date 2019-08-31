ef840b18e33fbdf16472b10e9b924922940b09f2
Holger Mueller <9jholger@gmail.com>
2019-08-31 14:22:05 -0500

    added expansion panles

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,10 +16,11 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -34,13 +35,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: `lfh3exftquq8`,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-contentful`,
+    //   options: {
+    //     spaceId: `lfh3exftquq8`,
+    //     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,39 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import ExpansionPanel from "@material-ui/core/ExpansionPanel"
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary"
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails"
+import Typography from "@material-ui/core/Typography"
+import Divider from "@material-ui/core/Divider"
+//import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 export default class BlogPage extends React.Component {
   render() {
     return (
       <Layout>
         <SEO title="BLOG" />
-        <h2>Story Archive</h2>
-        <h2>Blog Archive</h2>
+        <ExpansionPanel>
+          <ExpansionPanelSummary>
+            <h2>Story Archive</h2>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <p>Read stories!!</p>
+            <Divider variant="middle" />
+            <p>Link goes here:</p>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+
+        <ExpansionPanel>
+          <ExpansionPanelSummary>
+            <h2>Blog Archive</h2>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            Older Blogs if you want to take a deep dive.
+            <Divider variant="middle" />
+            <p>Link goes here:</p>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
       </Layout>
     )
   }
